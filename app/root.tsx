4970f1b68d64b790b7638a86ad6694a7cb8fa30b
roblesdotdev <httpsnip@gmail.com>
2024-08-19 21:18:42 -0300

    setup format rules with prettier

--- app/root.tsx
+++ app/root.tsx
@@ -4,25 +4,25 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
-import "~/styles/global.css";
-import { LinksFunction, MetaFunction } from "@remix-run/node";
-import faviconAssetUrl from "~/assets/favicon.svg";
+} from '@remix-run/react'
+import '~/styles/global.css'
+import { LinksFunction, MetaFunction } from '@remix-run/node'
+import faviconAssetUrl from '~/assets/favicon.svg'
 
 export const links: LinksFunction = () => [
-  { rel: "icon", type: "image+svg", href: faviconAssetUrl },
-];
+  { rel: 'icon', type: 'image+svg', href: faviconAssetUrl },
+]
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "WIP Journal" },
+    { title: 'WIP Journal' },
     {
-      name: "description",
+      name: 'description',
       content:
-        "Your space for recording and reflecting on your project progress.",
+        'Your space for recording and reflecting on your project progress.',
     },
-  ];
-};
+  ]
+}
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -39,9 +39,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Scripts />
       </body>
     </html>
-  );
+  )
 }
 
 export default function App() {
-  return <Outlet />;
+  return <Outlet />
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
-    <div className="font-sans p-4">
+    <div className="p-4 font-sans">
       <h1>Working...</h1>
     </div>
-  );
+  )
 }
